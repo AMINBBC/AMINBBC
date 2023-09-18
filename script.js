eb7dfa016467378bdaabdf5150371f4f87dc637b
پابلو امیلیو اسکوبار <111647971+AMINBBC@users.noreply.github.com>
2023-09-18 18:57:34 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1089,3 +1089,160 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+
+// ------------------------------93-----------------------------
+
+
+ var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// ------------------------------94-----------------------------
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// ------------------------------95-----------------------------
+
+// zoom
+
+//  function imageZoom(imgID, resultID) {
+//   var img, lens, result, cx, cy;
+//   img = document.getElementById(imgID);
+//   result = document.getElementById(resultID);
+//   /*create lens:*/
+//   lens = document.createElement("DIV");
+//   lens.setAttribute("class", "img-zoom-lens");
+//   /*insert lens:*/
+//   img.parentElement.insertBefore(lens, img);
+//   /*calculate the ratio between result DIV and lens:*/
+//   cx = result.offsetWidth / lens.offsetWidth;
+//   cy = result.offsetHeight / lens.offsetHeight;
+//   /*set background properties for the result DIV:*/
+//   result.style.backgroundImage = "url('" + img.src + "')";
+//   result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
+//   /*execute a function when someone moves the cursor over the image, or the lens:*/
+//  lens.addEventListener("mousemove", moveLens);
+//   img.addEventListener("mousemove", moveLens);
+//   /*and also for touch screens:*/
+//   lens.addEventListener("touchmove", moveLens);
+//   img.addEventListener("touchmove", moveLens);
+//   function moveLens(e) {
+//     var pos, x, y;
+//     /*prevent any other actions that may occur when moving over the image:*/
+//     e.preventDefault();
+//     /*get the cursor's x and y positions:*/
+//     pos = getCursorPos(e);
+//     /*calculate the position of the lens:*/
+//     x = pos.x - (lens.offsetWidth / 2);
+//     y = pos.y - (lens.offsetHeight / 2);
+//     /*prevent the lens from being positioned outside the image:*/
+//     if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
+//     if (x < 0) {x = 0;}
+//     if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
+//     if (y < 0) {y = 0;}
+//     /*set the position of the lens:*/
+//     lens.style.left = x + "px";
+//     lens.style.top = y + "px";
+//     /*display what the lens "sees":*/
+//     result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+//   }
+//   function getCursorPos(e) {
+//     var a, x = 0, y = 0;
+//     e = e || window.event;
+//     /*get the x and y positions of the image:*/
+//     a = img.getBoundingClientRect();
+//     /*calculate the cursor's x and y coordinates, relative to the image:*/
+//     x = e.pageX - a.left;
+//     y = e.pageY - a.top;
+//     /*consider any page scrolling:*/
+//     x = x - window.pageXOffset;
+//     y = y - window.pageYOffset;
+//     return {x : x, y : y};
+//   }
+// }
+
+
+
+
+
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+// ---------------------------------------------96------------------------------
+// setinterval
+
+// let f = 10
+
+// let timer = setInterval(function(){
+
+//   if (f === 0){
+//     alert("game over")
+//     clearInterval(timer)
+//   }
+// console.log(f)
+// f--
+
+// }, 1000)
+
+// -------------------------------------------------93 sabz --------------------------------------
+// make a timer
+
+// let min = prompt("enter min ")
+// let sec = prompt("enter sec ")
+
+// let timer = setInterval(function(){
+//     if (sec === -1){
+//         min--
+//         sec = 59
+
+//     }
+
+//     if (min === 0 && sec === 0){
+//         alert("finish")
+//         clearInterval(timer)
+//     }
+
+//     console.log( "timer:" + min +":" + sec)
+//     sec --
+
+// }, 1)
