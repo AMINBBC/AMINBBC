1e0d81c54448a550a3df2660d9b5bbafdc8fa4e3
amin <111647971+AMINBBC@users.noreply.github.com>
2024-03-24 16:20:08 +0330

    Add files via upload

---
+++ cppuni.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+using namespace std;
+
+//------------------------/ave two numbers*******---------------------------
+//int main(){
+//	
+//int a,b ;
+//float ave ;
+//cout<<"\n 2 adad sahih vared kon : " ;
+//cin>>a>>b ;
+//ave=(a+b)/2;
+//cout<<"\n miyanghin= " << ave <<endl ;
+//cout<<"---------------------------------";
+//ave=(float)(a+b)/2;
+//cout<<"\n miyanghin= " << ave <<endl ;
+//
+//}
+//----------------------------------مساحت و محیط مستطیل------------------------------------------
+
+//int main(){
+//	
+//int tol,arz;
+//cout<<"\ntol , arz ra vared kon:\n";
+//cin>>tol>>arz;
+//cout<<"**********************************"<<endl;
+//cout<<"\nmasahat = "<<tol*arz;
+//cout<<"\nmohit = "<<(tol+arz)*2;
+//
+//	
+//}
+
+//-----------------------------------------------------تفسیم و ضرب و جمع و تفری -------------------------------------
+
+//int main(){
+//	
+//	int a,b;
+//	cout<<"give me number";
+//	cin>>a>>b;
+//	cout<<a*b<<"\n";
+//
+//	cout<<a/b<<'\n';
+//		
+//	cout<<a-b<<"\n";
+//	
+//	cout<<a+b<<"\n";
+//	
+//}
+//------------------------------------------------------rabeteye--------------------------------
+
+
+
+//--------------------------------------jame do adad-----------------------------------
+//int main(){
+//	
+//int a , y , d ;
+//
+//cout<<"n yek adad 2 raghami vared kon : ";
+//
+//cin>>a ;
+//
+//y = a% 10;
+//
+//d =a/ 10;
+//
+//cout<<y+d;
+//
+//}
+
+//----------------------------------maqlob do adad---------------------------------------------------
+
+//int main(){
+//	
+//	int a , y , d , rev ;
+//
+//cout<<"n yek adad 2 raghami vared kon : ";
+//
+//cin >>a ;
+//
+//y = a%10;
+//
+//d = a/10;
+//
+//rev = y*10 + d;
+//
+//cout<< rev;
+//
+//}
+
+
+//----------------------------------------maqlob se  raqami------------------------------------------
+
+
+
+//int main() {
+//    int num, a = 0, b;
+//    cout << "Enter a three-digit number: ";
+//    cin >> num;
+//    while(num != 0) {
+//        b = num % 10;
+//        a = a * 10 + b;
+//        num /= 10;
+//    }
+//    cout << "Reversed number: " << a;
+//    return 0;
+//}
+//
+
+
+//----------------if ****if else*****else if *****switch******-------------------------------------------
+//
+//int main(){
+//	
+//	int a;
+//	cin>>a;
+//	if(a>0)
+//	cout<<"yes it is";
+//	else
+//	cout<<"no";
+//}
+
+
+
+//int main(){
+//	
+//int a , b , sum;
+//
+//cin>>a>>b;
+//
+//if(a<b){
+//	sum = a;
+//	a = b;
+//	b =sum;
+//}	
+//	cout<<a<<b;
+//}
+
+
+//int main(){
+//	int a;
+//	cin>>a;
+//	if(a%2==0){
+//		cout<<a*2;
+//	}
+//	else
+//	cout<<a*3;
+//	}
+
+//int main(){
+//	int a, b;
+//	cin>>a>>b;
+//	if(a%b==0){
+//		cout<<"yes";
+//	}
+//	
+//	else
+//	cout<<"no";
+//}
+
+//
+//int main(){
+//	int a;
+//	cin>>a;
+//	
+//	if(a <= 20 && a >=15){
+//			cout<<"A";
+//	}
+//
+//	
+//	else if (a <= 14 && a >= 10){
+//		cout<<"B";
+//	}
+//	
+//    
+//    
+//    else if (a <= 9 && a >= 5){
+//    	cout<<"C";
+//	}
+//    
+//    
+//    else if(a <= 4 && a >= 0){
+//    	cout<<"D";
+//	}
+//    
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
